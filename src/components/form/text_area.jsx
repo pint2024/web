8aa180c55bc80c5ed1f2d2889793aab5053bd6a3
Francisco Meneses <marques3033@gmail.com>
2024-02-29 11:02:31 +0000

    Create text_area.jsx
    facil meus caros

---
+++ src/components/form/text_area.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export function CaixaTexto({
+	title,
+	prefix = false,
+	inputType = "text",
+	handleChange,
+	value: propValue = "",
+	disabled = false,
+	placeholder = ''
+}) {
+	const [value, setValue] = useState(propValue);
+
+	const handleInputChange = (e) => {
+		setValue(e.target.value);
+		console.log(e.target.value)
+		if (typeof handleChange === 'function') {
+			handleChange(e);
+		}
+	};
+
+	return (
+		<div className="TextBox">
+			{title && <label htmlFor="inputNome">{title}</label>}
+			<div className="input-group">
+				{prefix && (
+					<span className="input-group-text" id="basic-addon">
+						{prefix}
+					</span>
+				)}
+				<input
+					type={inputType}
+					className="form-control"
+					id="inputNome"
+					onChange={handleInputChange}
+					value={value}
+					disabled={disabled}
+				/>
+			</div>
+		</div>
+	);
+}
