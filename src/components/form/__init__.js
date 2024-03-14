767596a4f03c466765af3ca0f9202ee87a1a81ef
Francisco Meneses <marques3033@gmail.com>
2024-03-14 09:23:06 +0000

    Update __init__.js
    fuck u

--- src/components/form/__init__.js
+++ src/components/form/__init__.js
@@ -6,7 +6,7 @@ export { DatePicker } from "./datePicker";
 export { DatePickerRange } from "./datePickerRange";
 export { FileBox } from "./fileBox";
 export { ImageBox } from "./imageBox";
-export { MultiSelectBox } from "./multiSelectBox";
+export { MultiSelectBox } from "./multiselectbox";
 export { OTPVerification } from "./OTPverification";
 export { TextArea } from "./textArea";
 export { TimePicker } from "./TimePicker";
