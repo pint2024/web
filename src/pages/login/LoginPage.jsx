d02cba35649c5dc2eb19e59b46ccc548e1fcdfe7
Francisco Meneses <marques3033@gmail.com>
2024-03-14 10:11:03 +0000

    .
    loginPAge madj

---
+++ src/pages/login/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './login';
+
+
+export function LoginPage() {
+  return (
+    <div className="login-page-container">
+      <Login />
+    </div>
+  );
+}
+
