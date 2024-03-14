cf1dd92c0379dc45dcc78570e4bc2b9e95320eb3
Francisco Meneses <marques3033@gmail.com>
2024-03-14 09:30:43 +0000

    Update comboBox.jsx
    fala baixo nengue

--- src/components/form/comboBox.jsx
+++ src/components/form/comboBox.jsx
@@ -28,7 +28,7 @@ export function ComboBox({
 				disabled={disabled}
 			>
 				<option value="placeHolder">{placeholder}</option>
-				{options.map((option, index) => (
+				{options?.map((option, index) => (
 					<option key={index} value={option.value}>
 						{option.label}
 					</option>
