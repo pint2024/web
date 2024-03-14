c2e5009e7d55f3e0080c92c9ceff75b3cde8ac47
Daniel <danielvalpereiro@gmail.com>
2024-03-14 21:57:32 +0000

    .

--- src/components/form/fileBox.jsx
+++ src/components/form/fileBox.jsx
@@ -1,36 +1,36 @@
 import React, { useState } from "react";
 
 export function FileBox({
-	title,
-	handleChange,
-	value: propValue = "",
-	disabled = false,
-	placeholder = ''
+  title,
+  handleChange,
+  value: propValue = "",
+  disabled = false,
+  placeholder = "",
 }) {
-	const [value, setValue] = useState(propValue);
+  const [value, setValue] = useState(propValue);
 
-	const handleFileChange = (e) => {
-		setValue(e.target.files[0]);
-		if (typeof handleChange === 'function') {
-			handleChange(e);
-		}
-	};
+  const handleFileChange = (e) => {
+    setValue(e.target.files[0]);
+    if (typeof handleChange === "function") {
+      handleChange(e);
+    }
+  };
 
-	return (
-		<div className="FileBox">
-			{title && <label htmlFor="fileInput">{title}</label>}
-			<div className="input-group">
-				<input
-					type="file"
-					className="form-control"
-					id="fileInput"
-					onChange={handleFileChange}
-					disabled={disabled}
-				/>
-				<label htmlFor="fileInput" className="input-group-text">
-					{value.name || placeholder}
-				</label>
-			</div>
-		</div>
-	);
+  return (
+    <div className="FileBox">
+      {title && <label htmlFor="fileInput">{title}</label>}
+      <div className="input-group">
+        <input
+          type="file"
+          className="form-control"
+          id="fileInput"
+          onChange={handleFileChange}
+          disabled={disabled}
+        />
+        <label htmlFor="fileInput" className="input-group-text">
+          {value.name || placeholder}
+        </label>
+      </div>
+    </div>
+  );
 }
