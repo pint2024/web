467982505153f7ee105cfef5cda035568448d79c
Francisco Meneses <marques3033@gmail.com>
2024-03-21 10:58:12 +0000

    edit do manute
    edit do manute

--- src/pages/autenticacao/iniciarSessao/iniciarSessao.jsx
+++ src/pages/autenticacao/iniciarSessao/iniciarSessao.jsx
@@ -15,26 +15,32 @@ export function IniciarSessao() {
 	};
 
 	return (
-		<div>
-			<form>
-				<CaixaTexto
-					handleChange={(e) => setFormData({ ...formData, login: e })}
-					value={formData.login}
-					label="Email ou Tag"
-				/>
-				<CaixaTexto
-					handleChange={(e) => setFormData({ ...formData, senha: e })}
-					value={formData.senha}
-					label="Senha"
-					type="password"
-				/>
-				<Botao handleClick={handleChange}>Entrar</Botao>
-			</form>
+		<div className="iniciar-sessao-container">
+			
+				<form>
+				<div className="form-container">
+					<CaixaTexto
+						handleChange={(e) => setFormData({ ...formData, login: e })}
+						value={formData.login}
+						label="Email ou Tag"
+					/>
+					<CaixaTexto
+						handleChange={(e) => setFormData({ ...formData, senha: e })}
+						value={formData.senha}
+						label="Senha"
+						type="password"
+					/>
+					</div>
+					<div className="form-container mt-1">
+					<Botao handleClick={handleChange} >Entrar</Botao>
+					</div >
+				</form>
 
-			<Botao variant="secundario" route="/criar-conta">
-				Registrar
-			</Botao>
-			<a href="/forgot-password">Esqueceu-se da senha?</a>
+				<Botao variant="secundario" route="/criar-conta" >
+					Registrar
+				</Botao>
+				<a href="/forgot-password">Esqueceu-se da senha?</a>
+			
 		</div>
 	);
 }
