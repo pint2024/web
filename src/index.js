9f152f08435a5d2c8e956bcf127e34e7ceb858cc
Lucas Sebastião <75097274+lmsebastiao@users.noreply.github.com>
2024-07-03 13:13:12 +0000

    estrutura (build & public)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-
-/// Componentes
+import ReactDOM from "react-dom";
 import App from "./App";
-
-/// Providers
 import { ToastContainer } from "react-toastify";
 import { LoadingProvider } from "context/loadingContext";
 import { AutenticacaoProvider } from "hooks/useAutenticacao";
 import { render } from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 
-/// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/js/bootstrap.min.js";
-
-/// Toastify
 import "react-toastify/dist/ReactToastify.css";
-
-/// Global Styles
 import "assets/styles/main.css";
 
 render(
