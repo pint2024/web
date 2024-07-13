2cc912d0a48bfc31af9b4fbf86c5abb8f5e371ab
Lucas Sebastião <75097274+lmsebastiao@users.noreply.github.com>
2024-07-13 15:25:24 +0100

    feito?

--- src/pages/PaginaInicial.jsx
+++ src/pages/PaginaInicial.jsx
@@ -1,7 +1,35 @@
-export function PaginaInicial() {
+import React from "react";
+import "./pagina-inicial.css";
+import CitiesImagem from "assets/images/cities.png"
+import FriendsImagem from "assets/images/friends.png"
+import RecomendacoesImagem from "assets/images/recomendation.png"
+
+export const PaginaInicial = () => {
 	return (
-		<>
-			Oi
-		</>
+		<div className="home-container">
+			<section className="hero-section">
+				<h1>Bem-vindo à Softinsa</h1>
+				<p>
+					Explore e descubra o melhor de cada cidade, integrando-se mais facilmente na região e na nossa organização.
+				</p>
+			</section>
+			<section className="features-section">
+				<div className="feature-card">
+					<img src={CitiesImagem} alt="Explorar Cidades" />
+					<h2>Explorar Cidades</h2>
+					<p>Descubra recomendações de restaurantes, habitação, desporto, comércio, lazer e muito mais.</p>
+				</div>
+				<div className="feature-card">
+					<img src={FriendsImagem} alt="Integração Fácil" />
+					<h2>Integração Fácil</h2>
+					<p>Conecte-se com colegas e participe em atividades extralaborais para uma integração rápida e fácil.</p>
+				</div>
+				<div className="feature-card">
+					<img src={RecomendacoesImagem} alt="Recomendações" />
+					<h2>Recomendações</h2>
+					<p>Partilhe e descubra recomendações de locais para comer, desportar, relaxar e muito mais.</p>
+				</div>
+			</section>
+		</div>
 	);
-}
+};
--- src/pages/conta/editar/ContaEditar.jsx
+++ src/pages/conta/editar/ContaEditar.jsx
@@ -1,27 +1,45 @@
 import { ApiRequest } from "api";
-import { Botao, ImageBox, Imagem } from "components";
+import { Botao, CaixaTexto, ImageBox, Imagem } from "components";
 import { useCarregando } from "hooks/useCarregando";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import UtilizadorDefault from "assets/images/user-default.png";
 import { useUserValidation } from "hooks/useAuth";
 import { Validador } from "utils/validator";
+import { Utils } from "utils/utils";
+import { ImagemUtilizador } from "components/common/imagem/ImagemUtilizador";
 
 export function ContaEditar() {
 	const { id } = useParams();
+	const { startLoading, stopLoading } = useCarregando();
+	const { userData, isValid } = useUserValidation(true);
+	const navigate = useNavigate();
+	const [erros, setErros] = useState([]);
+
 	const [dataConta, setdataConta] = useState(null);
 	const [newImagens, setnewImagens] = useState([]);
-	const [erros, setErros] = useState([]);
-	const { startLoading, stopLoading } = useCarregando();
-	const { userData, isValid } = useUserValidation();
-	const navigate = useNavigate();
+	const [formNome, setformNome] = useState([]);
+	const [formSobrenome, setformSobrenome] = useState([]);
+	const [formLinkedin, setformLinkedin] = useState([]);
+	const [formInstagram, setformInstagram] = useState([]);
+	const [formFacebook, setformFacebook] = useState([]);
 
 	useEffect(() => {
 		if (!isValid) return;
-		if (userData.id !== id) {
+		if (userData.id !== Utils.convertoStrToInt(id)) {
 			navigate("/");
 		}
 	}, [userData, isValid, id]);
+
+	useEffect(() => {
+		if (!dataConta) return;
+		setnewImagens(dataConta.imagem);
+		setformNome(dataConta.nome);
+		setformSobrenome(dataConta.sobrenome);
+		setformLinkedin(dataConta.linkedin);
+		setformInstagram(dataConta.instagram);
+		setformFacebook(dataConta.facebook);
+	}, [dataConta]);
 
 	useEffect(() => {
 		fetchData();
@@ -58,13 +76,85 @@ export function ContaEditar() {
 		stopLoading();
 	};
 
-	if (!dataConta) return;
+	const handleDataAtualizar = async () => {
+		const esquema = {
+			nome: { required: true },
+			sobrenome: { required: true },
+			linkedin: { required: true },
+			instagram: { required: true },
+			facebook: { required: true },
+		};
+
+		const validador = new Validador(esquema);
+		const data = {
+			nome: formNome,
+			sobrenome: formSobrenome,
+			linkedin: formLinkedin,
+			instagram: formInstagram,
+			facebook: formFacebook,
+		};
+
+		const validacao = validador.validar(data);
+		setErros(validacao);
+		if (!validador.isValido(validacao)) return;
+
+		startLoading();
+		await ApiRequest.atualizar("utilizador", userData.id, data);
+		await fetchData();
+		stopLoading();
+	};
+
+	if (!dataConta || !userData) return;
 
 	return (
 		<>
-			<Imagem src={dataConta.imagem ? dataConta.imagem : UtilizadorDefault} className="image-size circular-image" />
-			<ImageBox handleChange={setnewImagens} />
-			<Botao onClick={handleImageUpload}>Upload</Botao>
+			<div>
+				<ImagemUtilizador src={newImagens} className="image-size circular-image" />
+				<div className="d-flex">
+					<ImageBox handleChange={setnewImagens} />
+					<Botao onClick={handleImageUpload}>Upload</Botao>
+				</div>
+			</div>
+			<div>
+				<CaixaTexto
+					className="mt-2 me-auto"
+					handleChange={(e) => setformNome(e.target.value)}
+					value={formNome}
+					isInvalid={erros.nome}
+					label="Nome"
+				/>
+				<CaixaTexto
+					className="mt-2 me-auto"
+					handleChange={(e) => setformSobrenome(e.target.value)}
+					value={formSobrenome}
+					isInvalid={erros.sobrenome}
+					label="Sobrenome"
+				/>
+				<CaixaTexto
+					className="mt-2 me-auto"
+					handleChange={(e) => setformLinkedin(e.target.value)}
+					value={formLinkedin}
+					isInvalid={erros.linkedin}
+					label="Linkedin"
+				/>
+				<CaixaTexto
+					className="mt-2 me-auto"
+					handleChange={(e) => setformFacebook(e.target.value)}
+					value={formFacebook}
+					isInvalid={erros.facebook}
+					label="Facebook"
+				/>
+				<CaixaTexto
+					className="mt-2 me-auto"
+					handleChange={(e) => setformInstagram(e.target.value)}
+					value={formInstagram}
+					isInvalid={erros.instagram}
+					label="Instagram"
+				/>
+				<Botao onClick={handleDataAtualizar} className="mt-2 me-auto">
+					Atualizar
+				</Botao>
+			</div>
 		</>
 	);
 }
